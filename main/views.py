542d1cf2c3598819c9d4a340d438ccccdafebe7b
moonshine17 <furaokolo@gmail.com>
2024-03-18 15:09:26 +0300

    pre git

--- main/views.py
+++ main/views.py
@@ -1,3 +1,17 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'title': 'Home',
+        'content': 'Главная страница интернет-магазина - HOME',
+        'list': ['first', 'second'],
+        'dict': {'first': 1},
+        'isAuthentificated': False
+    }
+
+    return render(request, 'main/index.html', context)
+
+def about(request):
+    return HttpResponse('About page')
