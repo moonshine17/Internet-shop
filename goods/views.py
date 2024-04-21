b7aaa841bbc08bbd5761aed35f57ea8dbc6d0930
moonshine17 <furaokolo@gmail.com>
2024-04-21 11:42:50 +0300

    Second pagination option - GET params

--- goods/views.py
+++ goods/views.py
@@ -1,19 +1,21 @@
 from django.core.paginator import Paginator
-from django.shortcuts import get_list_or_404, get_object_or_404, render
+from django.shortcuts import get_list_or_404, render
 from django.template import context
 
 from goods.models import Products
 
 
 # Create your views here.
-def catalog(request, category_slug, page=1):
+def catalog(request, category_slug):
+    page = request.GET.get('page', 1)
+
     if category_slug == "all":
         goods = Products.objects.all()
     else:
         goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
 
     paginator = Paginator(goods, 6)
-    current_page = paginator.page(page)
+    current_page = paginator.page(int(page))
 
     context = {
         "title": "Home catalog",
