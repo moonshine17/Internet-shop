a245a2c7e1cd00f7fabe4d830bea1e4c8b7e8f5c
moonshine17 <furaokolo@gmail.com>
2024-05-04 19:39:57 +0300

    Create order template and form prepared

--- users/models.py
+++ users/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
 # Create your models here.
 
 class User(AbstractUser):
     image = models.ImageField(upload_to='users/images', blank=True, null=True, verbose_name='Аватар')
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
 
     class Meta:
         db_table = 'user'
