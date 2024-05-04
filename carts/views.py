7ae1ec7a41c611cb10ebb6fc7f98adf2f095845a
moonshine17 <furaokolo@gmail.com>
2024-05-04 17:58:31 +0300

    Session - carts for anonymous user

--- carts/views.py
+++ carts/views.py
@@ -22,6 +22,21 @@ def cart_add(request):
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
+    else:
+        cart = Cart.objects.filter(
+            session_key=request.session.session_key,product=product
+        )
+        if cart.exists():
+            cart = cart.first()
+            if cart:
+                cart.quantity += 1
+                cart.save()
+        else:
+            Cart.objects.create(
+                session_key=request.session.session_key, product=product, quantity=1
+            )
+
+
     user_cart = get_user_carts(request)
     cart_items_html = render_to_string(
         'carts/includes/included_cart.html', {'carts': user_cart}, request=request
